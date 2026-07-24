6f5422383635e0d0fa74e48ae36d865ada7bc4fe
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:48:34 +0100

    feat(ui): add dynamic props to UserAvatar

--- app/components/ui/UserAvatar.tsx
+++ app/components/ui/UserAvatar.tsx
@@ -1,3 +1,7 @@
-export function UserAvatar() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface UserAvatarProps {
+  title?: string;
+}
+
+export function UserAvatar({ title = "UserAvatar" }: UserAvatarProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
