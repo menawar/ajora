a5a75d21e08b40dbab5345129f2f69fe96c808ef
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:49:44 +0100

    feat(ui): add dynamic props to PulseRing

--- app/components/ui/PulseRing.tsx
+++ app/components/ui/PulseRing.tsx
@@ -1,3 +1,7 @@
-export function PulseRing() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface PulseRingProps {
+  title?: string;
+}
+
+export function PulseRing({ title = "PulseRing" }: PulseRingProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
