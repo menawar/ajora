872dd58c62df37b1a6223997ae378ccb13856731
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:49:14 +0100

    feat(ui): add dynamic props to HeroSection

--- app/components/ui/HeroSection.tsx
+++ app/components/ui/HeroSection.tsx
@@ -1,3 +1,7 @@
-export function HeroSection() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface HeroSectionProps {
+  title?: string;
+}
+
+export function HeroSection({ title = "HeroSection" }: HeroSectionProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
