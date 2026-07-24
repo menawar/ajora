84902a0d9563ddb5761926fc6705def3a03f47e7
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:50:01 +0100

    feat(ui): add dynamic props to ShimmerText

--- app/components/ui/ShimmerText.tsx
+++ app/components/ui/ShimmerText.tsx
@@ -1,3 +1,7 @@
-export function ShimmerText() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface ShimmerTextProps {
+  title?: string;
+}
+
+export function ShimmerText({ title = "ShimmerText" }: ShimmerTextProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
