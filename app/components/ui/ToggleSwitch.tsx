5e4cfc78271d0ab5b76ee06325864a9f2f90e955
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:48:52 +0100

    feat(ui): add dynamic props to ToggleSwitch

--- app/components/ui/ToggleSwitch.tsx
+++ app/components/ui/ToggleSwitch.tsx
@@ -1,3 +1,7 @@
-export function ToggleSwitch() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface ToggleSwitchProps {
+  title?: string;
+}
+
+export function ToggleSwitch({ title = "ToggleSwitch" }: ToggleSwitchProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
