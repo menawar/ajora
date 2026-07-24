731b29a8e263377dfddff47e843345a54febb879
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:48:40 +0100

    feat(ui): add dynamic props to AccordionList

--- app/components/ui/AccordionList.tsx
+++ app/components/ui/AccordionList.tsx
@@ -1,3 +1,7 @@
-export function AccordionList() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface AccordionListProps {
+  title?: string;
+}
+
+export function AccordionList({ title = "AccordionList" }: AccordionListProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
