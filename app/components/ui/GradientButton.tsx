ee6772567952df25805b9dc82277bec0db21236f
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:48:22 +0100

    feat(ui): add dynamic props to GradientButton

--- app/components/ui/GradientButton.tsx
+++ app/components/ui/GradientButton.tsx
@@ -1,3 +1,7 @@
-export function GradientButton() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface GradientButtonProps {
+  title?: string;
+}
+
+export function GradientButton({ title = "GradientButton" }: GradientButtonProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
