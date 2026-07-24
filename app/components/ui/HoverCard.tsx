43eae441fad846351efce4ed14c5c66a2141b1f3
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:49:27 +0100

    feat(ui): add dynamic props to HoverCard

--- app/components/ui/HoverCard.tsx
+++ app/components/ui/HoverCard.tsx
@@ -1,3 +1,7 @@
-export function HoverCard() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface HoverCardProps {
+  title?: string;
+}
+
+export function HoverCard({ title = "HoverCard" }: HoverCardProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
