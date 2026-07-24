05b3af5a2ca156a8bdd3c690cedadb9067adc32c
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-24 10:49:03 +0100

    feat(ui): add dynamic props to TabNav

--- app/components/ui/TabNav.tsx
+++ app/components/ui/TabNav.tsx
@@ -1,3 +1,7 @@
-export function TabNav() {
-  return <div className="p-4 flex items-center justify-center"></div>;
+interface TabNavProps {
+  title?: string;
+}
+
+export function TabNav({ title = "TabNav" }: TabNavProps) {
+  return <div className="p-4 flex items-center justify-center">{title}</div>;
 }
