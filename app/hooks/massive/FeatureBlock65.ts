c9c7bef87b679b21df5372fa67a25d713e306f6f
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 14:03:47 +0100

    feat(hooks): enhance FeatureBlock65 capabilities

--- app/hooks/massive/FeatureBlock65.ts
+++ app/hooks/massive/FeatureBlock65.ts
@@ -1,2 +1,2 @@
 import { useState, useEffect } from 'react';
-export function useFeatureBlock65() { const [state, setState] = useState(null); useEffect(() => { setState(true) }, []); return state; }
+export function useFeatureBlock65(initialValue: any) { const [state, setState] = useState(initialValue); useEffect(() => { setState(true) }, []); return state; }
