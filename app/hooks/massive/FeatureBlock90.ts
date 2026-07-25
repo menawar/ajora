64c878cbb567e62d9494e2d90a5f165bd9826661
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 14:07:31 +0100

    style(hooks): add structure to FeatureBlock90

--- app/hooks/massive/FeatureBlock90.ts
+++ app/hooks/massive/FeatureBlock90.ts
@@ -1,1 +1,2 @@
-export function useFeatureBlock90() { return null; }
+import { useState } from 'react';
+export function useFeatureBlock90() { const [state, setState] = useState(null); return state; }
