bda6cc4fe7aa6b42f121d78f220308bf35583a1b
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 14:07:04 +0100

    feat(hooks): enhance FeatureBlock87 capabilities

--- app/hooks/massive/FeatureBlock87.ts
+++ app/hooks/massive/FeatureBlock87.ts
@@ -1,2 +1,2 @@
 import { useState, useEffect } from 'react';
-export function useFeatureBlock87() { const [state, setState] = useState(null); useEffect(() => { setState(true) }, []); return state; }
+export function useFeatureBlock87(initialValue: any) { const [state, setState] = useState(initialValue); useEffect(() => { setState(true) }, []); return state; }
