980dfeaf1709c4acdf5d8f9162275adcd4d56e1c
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:21:21 +0100

    feat(ui): implement typescript interfaces for SubtleButton

--- app/components/mature/SubtleButton.tsx
+++ app/components/mature/SubtleButton.tsx
@@ -1,3 +1,9 @@
-export function SubtleButton() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface SubtleButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function SubtleButton({ title = "SubtleButton", className = "", ...props }: SubtleButtonProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
