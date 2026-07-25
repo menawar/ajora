703d9ff88309bbf3c7bbdfa5b83cb4705881c10b
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:20:55 +0100

    feat(ui): implement typescript interfaces for FormLabel

--- app/components/mature/FormLabel.tsx
+++ app/components/mature/FormLabel.tsx
@@ -1,3 +1,9 @@
-export function FormLabel() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface FormLabelProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FormLabel({ title = "FormLabel", className = "", ...props }: FormLabelProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
