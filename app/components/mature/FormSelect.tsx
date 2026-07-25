6583373b4dd3b75cd78442c9e8957cef5e298156
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:20:35 +0100

    feat(ui): implement typescript interfaces for FormSelect

--- app/components/mature/FormSelect.tsx
+++ app/components/mature/FormSelect.tsx
@@ -1,3 +1,9 @@
-export function FormSelect() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface FormSelectProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FormSelect({ title = "FormSelect", className = "", ...props }: FormSelectProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
