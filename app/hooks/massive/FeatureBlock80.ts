c45715af6cd8229a2d47a1aa4fbcc8dae9e916e2
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 14:06:00 +0100

    style(hooks): add structure to FeatureBlock80

--- app/hooks/massive/FeatureBlock80.ts
+++ app/hooks/massive/FeatureBlock80.ts
@@ -1,1 +1,2 @@
-export function useFeatureBlock80() { return null; }
+import { useState } from 'react';
+export function useFeatureBlock80() { const [state, setState] = useState(null); return state; }
