b4ae944f83eaab2c97924c402219274b03ae7450
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:20:15 +0100

    feat(ui): implement typescript interfaces for Typography

--- app/components/mature/Typography.tsx
+++ app/components/mature/Typography.tsx
@@ -1,3 +1,9 @@
-export function Typography() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface TypographyProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function Typography({ title = "Typography", className = "", ...props }: TypographyProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
