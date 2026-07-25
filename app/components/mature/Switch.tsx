d320660cea78b74da4c8bca75da46b111717eb02
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:22:29 +0100

    feat(ui): implement typescript interfaces for Switch

--- app/components/mature/Switch.tsx
+++ app/components/mature/Switch.tsx
@@ -1,3 +1,9 @@
-export function Switch() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface SwitchProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function Switch({ title = "Switch", className = "", ...props }: SwitchProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
