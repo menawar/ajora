118f12a830ddee3880240f5d0b1f7387d103b283
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 14:02:26 +0100

    feat(hooks): enhance FeatureBlock56 capabilities

--- app/hooks/massive/FeatureBlock56.ts
+++ app/hooks/massive/FeatureBlock56.ts
@@ -1,2 +1,2 @@
 import { useState, useEffect } from 'react';
-export function useFeatureBlock56() { const [state, setState] = useState(null); useEffect(() => { setState(true) }, []); return state; }
+export function useFeatureBlock56(initialValue: any) { const [state, setState] = useState(initialValue); useEffect(() => { setState(true) }, []); return state; }
