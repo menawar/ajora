bbd0f2a58635cd1f91c8b62a9b9f0a36126e0a04
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:20:42 +0100

    feat(ui): implement typescript interfaces for FormCheckbox

--- app/components/mature/FormCheckbox.tsx
+++ app/components/mature/FormCheckbox.tsx
@@ -1,3 +1,9 @@
-export function FormCheckbox() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface FormCheckboxProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FormCheckbox({ title = "FormCheckbox", className = "", ...props }: FormCheckboxProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
