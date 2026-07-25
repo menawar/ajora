6a22262a27f241cd48a51f15aa2f8b60eb4f07dd
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:21:28 +0100

    feat(ui): implement typescript interfaces for OutlinedButton

--- app/components/mature/OutlinedButton.tsx
+++ app/components/mature/OutlinedButton.tsx
@@ -1,3 +1,9 @@
-export function OutlinedButton() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface OutlinedButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function OutlinedButton({ title = "OutlinedButton", className = "", ...props }: OutlinedButtonProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
