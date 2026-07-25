f589817393db8258d99d9b8bb8643db2fd71be07
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:22:15 +0100

    feat(ui): implement typescript interfaces for AvatarGroup

--- app/components/mature/AvatarGroup.tsx
+++ app/components/mature/AvatarGroup.tsx
@@ -1,3 +1,9 @@
-export function AvatarGroup() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface AvatarGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function AvatarGroup({ title = "AvatarGroup", className = "", ...props }: AvatarGroupProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
