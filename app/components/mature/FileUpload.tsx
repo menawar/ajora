9db603ff9e2e263667a0a0d8131d83d3b252b13b
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:23:25 +0100

    feat(ui): implement typescript interfaces for FileUpload

--- app/components/mature/FileUpload.tsx
+++ app/components/mature/FileUpload.tsx
@@ -1,3 +1,9 @@
-export function FileUpload() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface FileUploadProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FileUpload({ title = "FileUpload", className = "", ...props }: FileUploadProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
