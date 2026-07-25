eeac34f7352410fbd181f0410dba054271ea1aa6
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 13:55:56 +0100

    feat(ui): enhance FeatureBlock11 capabilities

--- app/components/massive/FeatureBlock11.tsx
+++ app/components/massive/FeatureBlock11.tsx
@@ -1,1 +1,9 @@
-export function FeatureBlock11() { return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>; }
+import React from 'react';
+
+export interface FeatureBlock11Props extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FeatureBlock11({ title = "FeatureBlock11", className = "", ...props }: FeatureBlock11Props) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
+}
