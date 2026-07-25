4314a55fdf31b009f6a08c569b04a551f6fcaa3f
agenes01 <adamsagnessambo@gmail.com>
2026-07-25 16:45:23 +0100

    fix(api): implement core logic and state for cacheControlManager

--- app/api/clean/cacheControlManager.ts
+++ app/api/clean/cacheControlManager.ts
@@ -1,3 +1,4 @@
 export function cacheControlManager(headers: Record<string, string>) {
-  return headers != null;
+  if (!headers) return false;
+  return 'authorization' in headers || 'Authorization' in headers;
 }
