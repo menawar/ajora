71f5bae6bf544e503f14579246d6b8f8c1c2a404
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 13:55:22 +0100

    feat(ui): enhance FeatureBlock7 capabilities

--- app/components/massive/FeatureBlock7.tsx
+++ app/components/massive/FeatureBlock7.tsx
@@ -1,1 +1,9 @@
-export function FeatureBlock7() { return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>; }
+import React from 'react';
+
+export interface FeatureBlock7Props extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FeatureBlock7({ title = "FeatureBlock7", className = "", ...props }: FeatureBlock7Props) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
+}
