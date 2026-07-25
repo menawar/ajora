6e9d45a0209b48e9a4b7bf07f930b85889688725
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:20:28 +0100

    feat(ui): implement typescript interfaces for FormInput

--- app/components/mature/FormInput.tsx
+++ app/components/mature/FormInput.tsx
@@ -1,3 +1,9 @@
-export function FormInput() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface FormInputProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FormInput({ title = "FormInput", className = "", ...props }: FormInputProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
