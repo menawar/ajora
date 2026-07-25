903624913af85984564821ea5e2db9e52215fdc7
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:21:08 +0100

    style(ui): apply mature typography and color tokens to ContextualMenu

--- app/components/mature/ContextualMenu.tsx
+++ app/components/mature/ContextualMenu.tsx
@@ -1,3 +1,3 @@
 export function ContextualMenu() {
-  return <div className="p-4 m-2 flex flex-col gap-2"></div>;
+  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
 }
