f36988097d247c898d7a61ec70984c4dc42b096d
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 13:59:32 +0100

    feat(ui): enhance FeatureBlock36 capabilities

--- app/components/massive/FeatureBlock36.tsx
+++ app/components/massive/FeatureBlock36.tsx
@@ -1,1 +1,9 @@
-export function FeatureBlock36() { return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>; }
+import React from 'react';
+
+export interface FeatureBlock36Props extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function FeatureBlock36({ title = "FeatureBlock36", className = "", ...props }: FeatureBlock36Props) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
+}
