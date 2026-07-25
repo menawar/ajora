866040c11fbc441901ba9eb2190cd58c69d093d1
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:23:49 +0100

    feat(ui): implement typescript interfaces for ErrorState

--- app/components/mature/ErrorState.tsx
+++ app/components/mature/ErrorState.tsx
@@ -1,3 +1,9 @@
-export function ErrorState() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface ErrorStateProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function ErrorState({ title = "ErrorState", className = "", ...props }: ErrorStateProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
