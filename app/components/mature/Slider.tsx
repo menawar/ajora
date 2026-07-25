42b7ff5931efe3068f2c23017889a08936019ece
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:22:36 +0100

    feat(ui): implement typescript interfaces for Slider

--- app/components/mature/Slider.tsx
+++ app/components/mature/Slider.tsx
@@ -1,3 +1,9 @@
-export function Slider() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function Slider({ title = "Slider", className = "", ...props }: SliderProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
