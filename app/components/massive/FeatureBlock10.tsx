961223a7515572cb791d01b61dcce4745fcca508
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 13:55:47 +0100

    feat(ui): add logic to FeatureBlock10

--- app/components/massive/FeatureBlock10.tsx
+++ app/components/massive/FeatureBlock10.tsx
@@ -1,1 +1,1 @@
-export function FeatureBlock10() { return <div className="p-4 m-2 flex flex-col gap-2"></div>; }
+export function FeatureBlock10() { return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>; }
