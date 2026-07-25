e62a846fe31f907128c66a2ea776c43be13c4962
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:22:57 +0100

    feat(ui): implement typescript interfaces for Modal

--- app/components/mature/Modal.tsx
+++ app/components/mature/Modal.tsx
@@ -1,3 +1,9 @@
-export function Modal() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function Modal({ title = "Modal", className = "", ...props }: ModalProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
