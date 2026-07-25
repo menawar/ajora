fd7be3ded14aaa3af0867520e534e9031806f955
Sylvester Menawar <sylvesternathan93@gmail.com>
2026-07-25 10:23:04 +0100

    feat(ui): implement typescript interfaces for Drawer

--- app/components/mature/Drawer.tsx
+++ app/components/mature/Drawer.tsx
@@ -1,3 +1,9 @@
-export function Drawer() {
-  return <div className="p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200"></div>;
+import React from 'react';
+
+export interface DrawerProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+}
+
+export function Drawer({ title = "Drawer", className = "", ...props }: DrawerProps) {
+  return <div className={`p-4 m-2 flex flex-col gap-2 text-gray-900 bg-white border border-gray-200 ${className}`} {...props}>{title}</div>;
 }
